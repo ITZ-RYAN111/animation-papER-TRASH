2b9e6ad484bc19d1c95ef1dad171c62c28c6d872
RYAN SINGH <71920588+ITZ-RYAN111@users.noreply.github.com>
2020-11-13 13:02:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,78 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj, paperObject,groundObject	
+var world,dustbinObj1,dustbinObj2;
+
+
+function preload(){
+ 
+    
+
+
+}
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	dustbinObj=new dustbin(1200,650);
+	//dustbinObj=new dustbin(1000,650);
+	//dustbinObj1=new dustbin(1100,500);
+	//dustbinObj2=new dustbin(1200,300);
+	paperObject=new paper(200,450,40);
+	groundObject=new ground(width/2,670,width,20);
+	
+	
+
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1200,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
+
+	Engine.run(engine);
+
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("white");
+ 
+ // dustbinObj1.display();
+  dustbinObj.display();
+  //dustbinObj2.display(); 
+  paperObject.display();
+  groundObject.display();
+  
+ 
+  
+  
+ 
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+    
+  	}
+}
+
+
+
+
+
